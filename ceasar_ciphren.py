5883f7a79e6719585e7490e7c0d3a5160574c79b
Ivan Rindos <ivan.rindos@gmail.com>
2024-12-01 14:27:37 +0100

    Add repair encode function

--- ceasar_ciphren.py
+++ ceasar_ciphren.py
@@ -15,6 +15,8 @@ while True:
 
 for letter in word:
     letter_index = alphabet.index(letter)
-    new_index = letter_index + int(shift) 
+    new_index = letter_index + int(shift)
+    if new_index >= 26:
+        new_index %= 26
     encode_word += alphabet[new_index]
-    print(encode_word)
+print(f"Encode word is: {encode_word}")
